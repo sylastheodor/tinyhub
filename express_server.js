063e827a45c4157d889575a1c2766628482f2352
Sylas Serné <its.sylas@gmail.com>
2021-07-06 20:28:30 +0000

    incredibly busted first draft of generateRandomString

--- express_server.js
+++ express_server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const morgan = require("morgan")
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs")
+
+app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,6 +31,39 @@ app.get("/urls/new", (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
   res.render("urls_show", templateVars);
+});
+
+function generateRandomString() {
+  /* so I could do a bunch of equations with a String.FromCharCode, but then I'd have to make a couple if statements to avoid the punctuation characters
+  What I'm gonna do instead, is make a very long array of all the characters, and then return a random number from 0-61 corresponding to a character in the array
+  Maybe this will get refactored out but it's just a beta baby we're just getting started.  I found a pretty dece method on stackoverflow but I'm gonna do
+  my own method first.  */
+  let result = ''
+  const randoChar = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+    'o', 'p', 'q', 'r', 's', 't', 'u', 
+    'v', 'w', 'x', 'y', 'z', 'A', 'B', 
+    'C', 'D', 'E', 'F', 'G', 'H', 'I', 
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 
+    'X', 'Y', 'Z'
+  ];
+  //defining the assignment number
+  let assNum = Math.floor(Math.random() * 62);
+
+
+  result += randoChar[assNum]
+
+
+}
+
+}
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
 
 app.get("/urls", (req, res) => {
