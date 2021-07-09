4eb79de3eb2af9425a7e2aee92e7961fcbef9799
Sylas Serné <its.sylas@gmail.com>
2021-07-09 03:04:40 +0000

    Edit and Delete need userauthentication to go through even thru cURL

--- express_server.js
+++ express_server.js
@@ -60,7 +60,6 @@ const emailChecker = (email) => {
       return true;
     }
   }
-  return false;
 };
 
 const pwChecker = (pw) => {
@@ -81,9 +80,39 @@ const findByEmail = (email) => {
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userId: "default" 
+  },
+  '9sm5xK': {
+    longURL: "http://www.google.com",
+    userId: "default"
+  } 
 };
+
+const urlsForUser = (id) => {
+  result = {}
+  for(let url in urlDatabase){
+    if(id === urlDatabase[url].userId){
+      result[url] = urlDatabase[url]['longURL']
+    }
+  }
+  return result
+}
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const validUrls = urlsForUser(req.cookies.user_id)
+  for(urls in validUrls){
+    if (req.params.shortURL === url) {
+      delete urlDatabase[req.params.shortURL];
+      res.redirect(`/urls/`)
+    }
+  }
+  console.log("not deleted")
+  res.send(405)
+  res.redirect(`/urls/`)
+});
+
 
 app.post("/login", (req, res) => {
   console.log('req.body.email:', req.body.email)
@@ -107,8 +136,6 @@ app.get("/login", (req, res) => {
   const templateVars = { user: req.cookies['user_id']}
   res.render(`login_page`, templateVars);
 });
-
-
 
 app.post("/register", (req, res) => {
   let userId = `${generateRandomString()}`;
@@ -141,37 +168,79 @@ app.get("/hello", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {user: req.cookies.user_id};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/register", (req, res) => {
-  res.render("register")
-})
+  res.render("register");
+});
 
 
-app.get('/urls', (req, res) => {
+
+app.get('/urls', (req, res) => { 
   const Youser = req.cookies.user_id 
-  const templateVars = { urls: urlDatabase, user: Youser }; //passing urlDatabase as "urls" so that the urls_index.ejs can access it as such
+  console.log('Youser:', Youser)
+  if(!req.cookies.user_id) {
+    const templateVars = { urls: null, user: null }; 
+    res.render('urls_index', templateVars);
+    return;
+  } else {
+  const templateVars = { urls: urlsForUser(req.cookies.user_id.id), user: Youser }; //passing urlDatabase as "urls" so that the urls_index.ejs can access it as such
   res.render('urls_index', templateVars);
+  }
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: req.cookies['user_id'] }; //passing the value shortURL and longURL into the urls_show.ejs.  That's how it has access to THOSE values.
-  res.render("urls_show", templateVars); //the values for this might be switched from how they should be...
+
+  //we get an object of urls that match the 'req.cookies.user_id.id' ID.  
+  //SO, if req.param.shortURL !== one of those URLS, we need to redirect, perhaps send a 403 or a 401
+  //
+
+  const validUrl = urlsForUser(req.cookies.user_id.id)
+  console.log('validUrl:', validUrl)
+  if(!req.cookies.user_id) {
+    const templateVars = { shortURL: null, longURL: null, user: null }; 
+    res.render('urls_show', templateVars);
+    return;
+  }
+  for(let url in validUrl) {
+    if(url === req.params.shortURL) {
+      const templateVars = { shortURL: req.params.shortURL, longURL: validUrl[req.params.shortURL], user: req.cookies['user_id'] }; 
+      res.render('urls_show', templateVars); 
+      return;
+    }
+  }
+  res.redirect('/urls')
+  res.sendStatus(401)
 });
 
 app.post('/urls/:id', (req, res) => {
   console.log('req.params.id', req.params.id) // this is the shortened URL
   console.log('req.body', req.body.longURL) // this is the EDITED input
   console.log('urlDatabase', urlDatabase)//why is the urlDatabase passed to here?  I should ask tomorrow. Or is it because it's already in the file??
+  
+  if (!req.cookies.user_id){
+    res.redirect(`'/login`);
+    return;
+  }  
   const input = req.body.longURL
   const urlKey = req.params.id
   if(!input) {
-    res.redirect(`/urls/${urlKey}`)
+    res.redirect(`/urls/${urlKey}`);
     return;
   }
-  urlDatabase[urlKey] = input
-  res.redirect(`/urls/${urlKey}`) 
+  const validUrls = urlsForUser(req.cookies.user_id)
+  for(urls in validUrls){
+    if (!req.params.id === url) {
+      res.send(405);
+      res.redirect(`/urls/`);
+      return;
+    }
+  }
+
+  urlDatabase[urlKey]['longURL'] = input;
+  res.redirect(`/urls/`); 
 });
 
 
@@ -186,12 +255,21 @@ app.post('/logout', (req, res) => {
 app.post('/urls', (req, res) => {
   const longURL = req.body.longURL; //This is how we access the actual FORM of it.  req.body would be the entire body section.
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = longURL;
+  console.log('req.cookies.user_id:', req.cookies.user_id.id)
+  console.log('urldatabase before:', urlDatabase)
+  urlDatabase[shortURL] = {}; 
+  urlDatabase[shortURL]['longURL'] = longURL;
+  urlDatabase[shortURL]['userId'] =  req.cookies.user_id.id
+  console.log('urldatabase after:', urlDatabase)
   res.redirect(`/urls/${shortURL}`);
 });
 
+//This only works with full http://www.websitesFormattedLikeThis.com
 app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  let longURL = urlDatabase[req.params.shortURL]['longURL'];
+  if (!longURL.startsWith('http')) {
+    longURL = 'http://' + longURL
+  }
   res.redirect(longURL);
 });
 
@@ -200,10 +278,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect(`/urls/`)
-});
 
 
 app.listen(PORT, () => {
